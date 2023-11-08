b3615a9fd37bc605a637579792901706b711ace4
DiegoG0477 <diego.14.13.lopez@gmail.com>
2023-11-07 20:54:36 -0600

    banner component

--- src/app/store/layout.js
+++ src/app/store/layout.js
@@ -1,12 +1,13 @@
 "use client";
-// import Nav from "@/components/Nav";
+import Nav from "@/components/organisms/Nav";
 import "../globals.css";
 import Script from "next/script";
+
 function StoreLayout({ children }) {
   return (
     <>
       <body>
-        {/* <Nav/> */}
+        <Nav/>
         {children}
         <Script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js" />
       </body>
