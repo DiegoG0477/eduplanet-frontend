b793470b32ca6542a0f4a2eb54db25575f542d05
DiegoG0477 <diego.14.13.lopez@gmail.com>
2023-11-09 23:52:36 -0600

    nav advance

--- src/components/organisms/Nav.jsx
+++ src/components/organisms/Nav.jsx
@@ -1,10 +1,25 @@
 import eduplanetLogo from "@/public/assets/logo-png-w.png";
 import Img from "next/image";
 
-export default function Nav(){
-    return(
+export default function Nav() {
+    return (
         <div className="nav">
-           <Img src={eduplanetLogo}/>
+            <Img src={eduplanetLogo} className="nav-logo" />
+            <div className="nav-links">
+                <ul>
+                    <li>Blog</li>
+                    <li>Store</li>
+                    <li>Multimedia</li>
+                    <li>Descuentos</li>
+                    <li>Cursos</li>
+                </ul>
+            </div>
+
+            <div className="auth-nav-section">
+                <button>Iniciar Sesión</button>
+                <button>Registrarse</button>
+                
+            </div>
         </div>
     );
 }
