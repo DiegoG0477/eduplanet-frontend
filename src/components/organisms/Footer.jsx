2917bf6117f149ff74b39e27153642b482257e2c
DiegoG0477 <diego.14.13.lopez@gmail.com>
2023-11-11 21:02:13 -0600

    footer was added

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,4 +1,5 @@
 import eduplanetLogo from "@/public/assets/logo-png-w.png";
+import Link from "next/link";
 import Img from "next/image";
 
 export default function Footer() {
@@ -17,23 +18,81 @@ export default function Footer() {
                         voluptates. Quaerat, voluptatum.
                     </p>
                     <p>Nuestras Redes Sociales</p>
-                    <i className="fa-brands fa-instagram social-icon-footer"></i>
-                    <i className="fa-brands fa-facebook social-icon-footer"></i>
-                    <i className="fa-brands fa-youtube social-icon-footer"></i>
+                    <div className="social-i-footer-container">
+                        <i className="fa-brands fa-instagram social-icon-footer"></i>
+                        <i className="fa-brands fa-facebook social-icon-footer"></i>
+                        <i className="fa-brands fa-youtube social-icon-footer"></i>
+                    </div>
                 </div>
 
                 <div>
                     <div className="footer-feature">
-                        <div className="feature-color-bar"></div> <p>Materiales</p>
+                        <div
+                            className="feature-color-bar"
+                            style={{ background: "#d41616" }}
+                        ></div>
+                        <p>Materiales</p>
                     </div>
-                    <div className="footer-feature"></div>
-                    <div className="footer-feature"></div>
-                    <div className="footer-feature"></div>
-                    <div className="footer-feature"></div>
+                    <div className="footer-feature">
+                        <div
+                            className="feature-color-bar"
+                            style={{ background: "#FAFF00" }}
+                        ></div>
+                        <p>Descuentos</p>
+                    </div>
+                    <div className="footer-feature">
+                        <div
+                            className="feature-color-bar"
+                            style={{ background: "#0029FF" }}
+                        ></div>
+                        <p>Videos</p>
+                    </div>
+                    <div className="footer-feature">
+                        <div
+                            className="feature-color-bar"
+                            style={{ background: "#FA00FF" }}
+                        ></div>
+                        <p>Blog</p>
+                    </div>
+                    <div className="footer-feature">
+                        <div
+                            className="feature-color-bar"
+                            style={{ background: "#6C3131" }}
+                        ></div>
+                        <p>Comentarios</p>
+                    </div>
                 </div>
 
-                <div className="payment-methods">
+                <div className="footer-third-column">
+                    <p>Métodos de pago</p>
+                    <div className="payment-methods">
+                        <div className="payment-pair">
+                            <i className="fa-brands fa-cc-visa payment-method"></i>
+                            <i className="fa-brands fa-cc-mastercard payment-method"></i>
+                        </div>
+                        <div className="payment-pair">
+                            <i className="fa-brands fa-cc-paypal payment-method"></i>
+                            <i className="fa-brands fa-cc-amex payment-method"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <p className="terms-text">© 2023 Eduplanet. Todos los derechos reservados.</p>
+
+            <div className="bottom-footer-content">
+                <div className="footer-links">
+                    <Link href="#" style={{ textDecoration: "none" }}>
+                        <p className="footer-link">Términos y condiciones</p>
+                    </Link>
+
+                    <Link href="#" style={{ textDecoration: "none" }}>
+                        <p className="footer-link">Política de privacidad</p>
+                    </Link>
+
+                    <Link href="#" style={{ textDecoration: "none" }}>
+                        <p className="footer-link">Contacto</p>
+                    </Link>
                 </div>
             </div>
         </footer>
